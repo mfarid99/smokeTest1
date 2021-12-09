06622e9f8abe4f1a54c5aa8adc553cf15ddbe34c
Moe <moe@triblio.com>
2021-12-09 11:35:18 -0500

    step 1

---
+++ .qawolf/myFirstTest.test.js
@@ -0,0 +1,26 @@
+const qawolf = require("qawolf");
+
+let browser;
+let context;
+
+beforeAll(async () => {
+  browser = await qawolf.launch();
+  context = await browser.newContext();
+  await qawolf.register(context);
+});
+
+afterAll(async () => {
+  await qawolf.stopVideos();
+  await browser.close();
+});
+
+test("myFirstTest", async () => {
+  const page = await context.newPage();
+  await page.goto("https://todomvc.com/examples/react/", { waitUntil: "domcontentloaded" });
+  await page.click(".new-todo");
+  await page.click(".new-todo");
+  await page.fill(".new-todo", "create test");
+  await page.press(".new-todo", "Enter");
+  await page.click(".toggle");
+  await page.click(".clear-completed");
+});
